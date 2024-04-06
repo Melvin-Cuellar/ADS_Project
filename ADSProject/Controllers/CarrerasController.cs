fba03cdcef3d309ad3cd4ba3c524ad61a2745bfa
Melvin-Cuellar <melvincuellar9801@gmail.com>
2024-04-06 00:34:04 -0600

    “Se_agrega_CRUD_para_Estudiantes_y_Carreras”

---
+++ ADSProject/Controllers/CarrerasController.cs
@@ -0,0 +1,156 @@
+﻿using ADSProject.Interfaces;
+using ADSProject.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ADSProject.Controllers
+{
+    [Route("api/carreras/")]
+    public class CarrerasController : ControllerBase
+    {
+        private readonly ICarrera carrera;
+        private const string COD_EXITO = "000000";
+        private const string COD_ERROR = "999999";
+        private string pCodRespuesta;
+        private string pMensajeUsuario;
+        private string pMensajeTecnico;
+
+        public CarrerasController(ICarrera carrera)
+        {
+            this.carrera = carrera;
+        }
+
+        [HttpPost("agregarCarrera")]
+        public ActionResult<string> AgregarCarrera([FromBody] Carrera carrera)
+        {
+            try
+            {
+                int contador = this.carrera.AgregarCarrera(carrera);
+
+                if (contador > 0)
+                {
+                    pCodRespuesta = COD_EXITO;
+                    pMensajeUsuario = "Registro insertado con éxito";
+                    pMensajeTecnico = pCodRespuesta + " || " + pMensajeUsuario;
+                }
+                else
+                {
+                    pCodRespuesta = COD_ERROR;
+                    pMensajeUsuario = "Ocurrio un error al insertar el registro";
+                    pMensajeTecnico = pCodRespuesta + " || " + pMensajeUsuario;
+                }
+
+                return Ok(new { pCodRespuesta, pMensajeUsuario, pMensajeTecnico });
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        [HttpPost("actualizarCarrera/{idCarrera}")]
+        public ActionResult<string> ActualizarCarrera(int idCarrera, [FromBody] Carrera carrera)
+        {
+            try
+            {
+                int contador = this.carrera.ActualizarCarrera(idCarrera, carrera);
+
+                if (contador > 0)
+                {
+                    pCodRespuesta = COD_EXITO;
+                    pMensajeUsuario = "Registro actualizado con éxito";
+                    pMensajeTecnico = pCodRespuesta + " || " + pMensajeUsuario;
+                }
+                else
+                {
+                    pCodRespuesta = COD_ERROR;
+                    pMensajeUsuario = "Ocurrio un error al actualizar el registro";
+                    pMensajeTecnico = pCodRespuesta + " || " + pMensajeUsuario;
+                }
+
+                return Ok(new { pCodRespuesta, pMensajeUsuario, pMensajeTecnico });
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+
+        [HttpDelete("eliminarCarrera/{idCarrera}")]
+        public ActionResult<string> EliminarCarrera(int idCarrera)
+        {
+            try
+            {
+                bool eliminado = this.carrera.EliminarCarrera(idCarrera);
+
+                if (eliminado)
+                {
+                    pCodRespuesta = COD_EXITO;
+                    pMensajeUsuario = "Registro eliminado con éxito";
+                    pMensajeTecnico = pCodRespuesta + " || " + pMensajeUsuario;
+                }
+                else
+                {
+                    pCodRespuesta = COD_ERROR;
+                    pMensajeUsuario = "Ocurrio un error al eliminar el registro";
+                    pMensajeTecnico = pCodRespuesta + " || " + pMensajeUsuario;
+                }
+
+                return Ok(new { pCodRespuesta, pMensajeUsuario, pMensajeTecnico });
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+
+        [HttpGet("obtenerCarreraPorId/{idCarrera}")]
+        public ActionResult<Carrera> ObtenerCarreraePorId(int idCarrera)
+        {
+            try
+            {
+                Carrera carrera = this.carrera.ObtenerCarreraPorId(idCarrera);
+
+                if (carrera != null)
+                {
+                    return Ok(carrera);
+                }
+                else
+                {
+                    pCodRespuesta = COD_ERROR;
+                    pMensajeUsuario = "No se encontro el registro";
+                    pMensajeTecnico = pCodRespuesta + " || " + pMensajeUsuario;
+
+                    return NotFound(new { pCodRespuesta, pMensajeUsuario, pMensajeTecnico });
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+
+        [HttpGet("obtenerEstudiantes")]
+        public ActionResult<List<Carrera>> ObtenerCarreras()
+        {
+            try
+            {
+                List<Carrera> lstCarreras = this.carrera.ObtenerTodasLasCarreras();
+
+                return Ok(lstCarreras);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+    }
+}
---
+++ ADSProject/Repositories/EstudianteRepository.cs
@@ -0,0 +1,104 @@
+﻿using ADSProject.Interfaces;
+using ADSProject.Models;
+
+namespace ADSProject.Repositories
+{
+    public class EstudianteRepository : IEstudiante
+    {
+        private List<Estudiante> lstEstudiantes = new List<Estudiante>
+        {
+            new Estudiante{ IdEstudiante = 1, NombresEstudiante = "JUAN CARLOS",
+                ApellidosEstudiante = "PEREZ SOSA", CodigoEstudiante = "PS24I04002",
+                CorreoEstudiante = "ps24I04002@usonsonate.edu.sv"}
+        };
+
+        public int AgregarEstudiante(Estudiante estudiante)
+        {
+            try
+            {
+                /* Validar si existen datos en la lista, de ser así, tomaremos el último ID
+                 y lo incrementamos en una unidad */
+                if (lstEstudiantes.Count > 0)
+                {
+                    estudiante.IdEstudiante = lstEstudiantes.Last().IdEstudiante + 1;
+                }
+
+                lstEstudiantes.Add(estudiante);
+
+                return estudiante.IdEstudiante;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public int ActualizarEstudiante(int idEstudiante, Estudiante estudiante)
+        {
+            try
+            {
+                // Obtenemos el indice del objeto a actualizar
+                int indice = lstEstudiantes.FindIndex(tmp => tmp.IdEstudiante == idEstudiante);
+
+                //Procedemos con la actualización
+                lstEstudiantes[indice] = estudiante;
+
+                return idEstudiante;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public bool EliminarEstudiante(int idEstudiante)
+        {
+            try
+            {
+                // Obtenemos el indice del objeto a eliminar
+                int indice = lstEstudiantes.FindIndex(tmp => tmp.IdEstudiante == idEstudiante);
+
+                //Procedemos con la eliminación del objeto
+                lstEstudiantes.RemoveAt(indice);
+
+                return true;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public Estudiante ObtenerEstudiantePorId(int idEstudiante)
+        {
+            try
+            {
+                // Buscamos y asignamos el objeto estudiante
+                Estudiante estudiante = lstEstudiantes.FirstOrDefault(tmp => tmp.IdEstudiante == idEstudiante);
+
+                return estudiante;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public List<Estudiante> ObtenerTodosLosEstudiantes()
+        {
+            try
+            {
+                return lstEstudiantes;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
