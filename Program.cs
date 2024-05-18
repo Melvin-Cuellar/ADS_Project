b22fbd001a10e011cb1ba282f846fe37e17884c1
Melvin-Cuellar <128654252+Melvin-Cuellar@users.noreply.github.com>
2024-05-18 20:14:14 +0200

    Add files via upload
    Guía 5 y 7 con algunos pendientes

---
+++ Program.cs
@@ -0,0 +1,47 @@
+using ADSProject.DB;
+using ADSProject.Interfaces;
+using ADSProject.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+//Configurando DBContext
+builder.Services.AddDbContext<ApplicationDbContext>(opciones => opciones.UseSqlServer("name=DefaultConnection"));
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//Configurando inyección de dependencias
+builder.Services.AddSingleton<IEstudiante, EstudianteRepository>();
+builder.Services.AddSingleton<ICarrera, CarreraRepository>();
+builder.Services.AddSingleton<IMateria, MateriaRepository>();
+builder.Services.AddSingleton<IProfesor, ProfesorRepository>();
+builder.Services.AddSingleton<IGrupo, GrupoRepository>();
+
+//Configurando CORS
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(configuration =>
+    {
+        configuration.WithOrigins(builder.Configuration["allowedOrigins"]!).AllowAnyMethod().AllowAnyHeader();
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
